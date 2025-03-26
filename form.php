e2b6707da0bdbf56c1c5a323e17413a3355f1e86
gilbertobuca <168492975+gilbertobuca@users.noreply.github.com>
2025-03-27 00:39:42 +0100

    1

--- form.php
+++ form.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina de login</title>
+    <link rel="stylesheet" href="form.css">
 </head>
 <body>
-    <form action="testlogin.php" method="POST">
-
-    
-    </form>
+    <div>
+        <h1>Login</h1>
+        <form action="testlogin.php" method="POST">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="senha" placeholder="Senha">
+            <input type="submit" name="submit" id="button" value="Enviar">
+        
+        </form>
+    </div>
     
 </body>
 </html>
